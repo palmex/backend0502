5e9cf839b7a9470a63fec3201a75696f5fb46241
Admin Server <apostlma@ucsd.edu>
2023-05-02 13:52:09 +0000

    first two endpoints

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
-
 const app = express()
 app.use(express.json())
 const port = 3000
 
+// Our listener endpoints
 app.get('/', (req,res) => {
+    console.log("root endpoint")
     res.status(200).json({"success": "your first API endpoint"})
 })
+
+app.get('/test', (req,res) => {
+    console.log("Im alive and well")
+    res.status(200).json({"test": "endpoint"})
+})
+// -----------------
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
