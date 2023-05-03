0674a400001866876b6f387cc1245ceac38630d5
Admin Server <apostlma@ucsd.edu>
2023-05-03 11:29:43 +0000

    added nodedemon

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+
+
+
 const app = express()
+
 app.use(express.json())
+
 const port = 3000
 
 // Our listener endpoints
-app.get('/', (req,res) => {
-    console.log("root endpoint")
-    res.status(200).json({"success": "your first API endpoint"})
+
+app.get('/name', ()=> {})
+app.post('/name', ()=> {})
+
+app.post('/', (request,response) => {
+    console.log(request.body)
+    response.status(200).json(request.body)
 })
 
 app.get('/test', (req,res) => {
