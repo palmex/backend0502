35ddd2c75d302a756e375b46d9d7aa6b43dc4237
Admin Server <apostlma@ucsd.edu>
2023-05-03 13:19:56 +0000

    added admin header on cars/all endpoint

--- routes/cars.js
+++ routes/cars.js
@@ -11,6 +11,25 @@ carsRouter.get('/test', (req,res)=>{
     res.status(200).json({"cars":"test"})
 })
 
+carsRouter.get('/all', (req,res)=> {
+
+    console.log(req.headers)
+    if(req.headers.admin){
+        const queryStatement = "SELECT * FROM cars;"
+        db.query(queryStatement, (error, results)=>{
+            if(error){
+                res.status(500).json(error)
+            } else {
+                result = results.rows 
+                res.status(200).json(result)
+            }
+        })
+    } else {
+        res.status(403).json({"unauthorized":"please use an admin header"})
+    }
+    
+})
+
 
 module.exports = carsRouter; 
 
